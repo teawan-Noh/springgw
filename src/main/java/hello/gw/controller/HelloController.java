b1ca52975263fdb0209f29bc92aacba55b84e5f0
DESKTOP-QA0M42J\taewan <ntw2100@gmail.com>
2022-03-08 20:50:02 +0900

    Controller - json/xml 출력 세팅중

--- src/main/java/hello/gw/controller/HelloController.java
+++ src/main/java/hello/gw/controller/HelloController.java
@@ -3,14 +3,22 @@ package hello.gw.controller;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,7 +27,7 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import hello.gw.dto.ArtistBaseInfo;
 import hello.gw.service.ArtistBaseInfoService;
 
-@Controller
+@RestController
 public class HelloController {
 	
 	public static final String APPLICATION_JSON_VALUE = "application/json";
@@ -43,58 +51,32 @@ public class HelloController {
 	}
 	
 	@GetMapping("hello-string")
-	@ResponseBody
 	public String helloString(@RequestParam("nm") String nm) {
 		return "hello "+nm; 
 	}
 	
-	@GetMapping("hello-all")
-	@ResponseBody
-	public List<ArtistBaseInfo> helloString(HttpServletResponse response) {
-		
+	@GetMapping(path = "hello-all", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	public List<ArtistBaseInfo> helloString(HttpServletResponse response, HttpServletRequest request) {
+		try {
+			if(request.getParameter("type").equals("json") || request.getParameter("type") == null) {
+				System.out.println("json 호출1 : " + request.getHeader("Accept")); 
+				response.setHeader("Accept", APPLICATION_JSON_VALUE);
+//				response.setContentType(APPLICATION_JSON_VALUE);
+				System.out.println("json 호출2 : " + request.getHeader("Accept")); 
+			}
+			else if(request.getParameter("type").equals("xml")) {
+				System.out.println("xml 호출1 : " + request.getHeader("Accept")); 
+				response.setHeader("Accept", APPLICATION_XML_VALUE);
+//				response.setContentType(APPLICATION_XML_VALUE);
+				System.out.println("xml 호출2 : " + request.getHeader("Accept")); 
+			}
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
 		return service.artistBaseInfoAll(); 
 	}
 	
-//	@GetMapping("hello-all")
-//	@ResponseBody
-//	public void helloString2(@RequestParam("type") String type,HttpServletResponse response) {
-//		//타입이 json일경우
-//		String resultType = type;
-//		//String resultType = "xml"
-//
-//		if("xml".equals(resultType)) {
-//
-//		response.setContentType("application/xml");
-//		response.setCharacterEncoding("UTF-8");
-//		try {
-//			response.getWriter().write(new XmlMapper().writeValueAsString(service.artistBaseInfoAll()));
-//		} catch (JsonProcessingException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		}
-//
-//		//타입이 json일경우
-//		else {
-//		response.setContentType("application/json");
-//		response.setCharacterEncoding("UTF-8");
-//		try {
-//			response.getWriter().write(new ObjectMapper().writeValueAsString(service.artistBaseInfoAll()));
-//		} catch (JsonProcessingException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		}
-//	}
-	
 	@GetMapping("hello-api")
-	@ResponseBody
 	public Hello helloApi(@RequestParam("nm") String nm) {
 		Hello hello = new Hello();
 		hello.setNm(nm);
